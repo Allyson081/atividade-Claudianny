1f7b91a737457000b7db7e15ba0003fb84735a21
Allyson081 <allysonhenrique20@hotmail.com>
2025-09-24 20:33:54 -0300

    Add files via upload

---
+++ Atividade 03 claudiany/main.py
@@ -0,0 +1,7 @@
+from veiculo import Moto
+minha_moto = Moto()
+minha_moto.definir_marca('Honda')
+minha_moto.definir_modelo('Africa Twin')
+
+print("marca:", minha_moto.marca)
+print("modelo:", minha_moto.modelo)
---
+++ Atividade 03 claudiany/veiculo.py
@@ -0,0 +1,8 @@
+class Moto:
+    marca = 'não definida'
+    modelo = 'não definida'
+    def definir_marca(self, nova_marca):
+        self.marca = nova_marca
+    def definir_modelo(self, novo_modelo):
+        self.modelo = novo_modelo
+        
